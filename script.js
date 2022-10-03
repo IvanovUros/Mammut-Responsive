8cc51d47b027aab1e1a050b794e28999e34d4f00
IvanovUros <urivanov03@gmail.com>
2022-10-03 16:34:10 +0200

    3/10/22

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
+const body = document.getElementById('body');
 const logoWrapper = document.getElementById('logoWrapper');
 const overheadBar = document.getElementById('overheadBar');
 const menuBtn = document.getElementById('menuBtn');
+const menuBtnBurgerTop = document.getElementById('menuBtnBurgerTop');
+const menuBtnBurgerMid = document.getElementById('menuBtnBurgerMid');
+const menuBtnBurgerBtm = document.getElementById('menuBtnBurgerBtm');
 const loopBasketWrapper = document.getElementById('loopBasketWrapper');
 const loopBasketItem = Array.from(document.getElementsByClassName('loopBasketItem'));
 const loopBasketItemElement = Array.from(document.getElementsByClassName('loopBasketItemElement'));
 const loopBasketItemElementWithBorder = Array.from(document.getElementsByClassName('loopBasketItemElementWithBorder'));
+const mainMenuOpenClosSwitch = document.getElementById('mainMenuOpenCloseSwitch');
+const mainMenu = document.getElementById('mainMenu');
 let screenWidth = screen.width;
-console.log(screenWidth);
 
+addEventListener('resize', () => {
+  screenWidth = screen.width;
+  console.log(screenWidth);
+  console.log(menuOpen);
+});
+function overflowHidden(x) {
+  x.style.overflow = 'hidden';
+}
+function overflowInitial(x) {
+  x.style.overflow = 'initial';
+}
 
+let menuOpen = false;
 if(screenWidth < 1024) {
 onload = () => {
   overheadBar.style.width = '90%';
@@ -41,4 +58,33 @@ setTimeout( function loopBasketItemAnimation() {
     loopBasketItem.forEach(loopBasketItem => {
     });
   },1000);
-}
+};
+
+
+menuBtn.addEventListener('click', () =>{
+  if (screenWidth < 1024) {
+    if (!menuOpen) {
+      menuBtn.classList.add('open');
+      menuOpen = true;
+      overflowHidden(body);
+    } else {
+      menuBtn.classList.remove('open');
+      menuOpen = false;
+      overflowInitial(body);
+    }
+  } else {
+    if (!menuOpen) {
+      menuBtn.classList.add('open-laptop');
+      menuOpen = true;
+      overflowHidden(body);
+    } else {
+      menuBtn.classList.remove('open-laptop');
+      menuOpen = false;
+      overflowInitial(body);
+    }
+  }
+});
+
+menuBtn.addEventListener('click', () => {
+  console.log(menuOpen);
+});
